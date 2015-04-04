0ac67ad99969f0831d71ca8a5bc186a16f307cb3
Alfonso <lorenzo_5_10@hotmail.com>
2015-04-03 20:52:12 -0600

    Correcciones en codigo de Carousel

--- application/views/head/librerias_view.php
+++ application/views/head/librerias_view.php
@@ -7,17 +7,22 @@
     <meta name="description" content="Proyecto T-SHIRT &amp; STAFF para la materia de Administracion de Proyectos">
     <meta name="author" content="Andrea Prott &amp; Alfonso Ramirez">
     <title>T-SHIRT &amp; STAFF</title>
+
+    <!-- Inicio librerias Bootstrap-->
     <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- Fin librerias Bootstrap-->
 
-    <!--<link href="<?=base_url()?>css/style.css" type="text/css" rel="stylesheet">-->
+    
+    <!--Inicio ibrerias JQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!--Fin ibrerias JQuery-->
+
 </head>
 
